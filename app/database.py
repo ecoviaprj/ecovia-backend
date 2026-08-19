20f0856e9cb75a755b243a6d21238f6cee2e945b
Asvithab <145272606+Asvithab@users.noreply.github.com>
2026-08-19 21:23:48 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
 import os
+import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+pymysql.install_as_MySQLdb()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
